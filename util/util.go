5eb3b98f3ea42e6cd8cacca785e9764624c44602
IamCathal <cathalsocallaghan@gmail.com>
2022-11-03 21:19:13 +0000

    Successfully send correct message format for discord

--- util/util.go
+++ util/util.go
@@ -9,11 +9,12 @@ import (
 	"os"
 	"time"
 
+	"github.com/iamcathal/booksbooksbooks/db"
 	"github.com/iamcathal/booksbooksbooks/dtos"
 )
 
 func SendNewBookIsAvailableMessage(book dtos.TheBookshopBook) {
-	smallerStyleMessage := dtos.DiscordMsg{
+	message := dtos.DiscordMsg{
 		Username:   "BooksBooksBooks",
 		Avatar_url: "https://cathaloc.dev/static/favicons/ms-icon-150x150.png",
 		Embed: []dtos.DiscordEmbed{{
@@ -38,41 +39,16 @@ func SendNewBookIsAvailableMessage(book dtos.TheBookshopBook) {
 			},
 		}},
 	}
-	DeliverWebHook(smallerStyleMessage, getDefaultWebhookURL())
 
-	biggerStyleMessage := dtos.DiscordMsg{
-		Username:   "BooksBooksBooks",
-		Avatar_url: "https://cathaloc.dev/static/favicons/ms-icon-150x150.png",
-		Embed: []dtos.DiscordEmbed{{
-			Author: dtos.EmbedAuthor{
-				Name:    "Powered by BooksBooksBooks",
-				IconURL: "https://cathaloc.dev/static/favicons/ms-icon-150x150.png",
-				URL:     "https://github.com/IamCathal/BooksBooksBooks",
-			},
-			Title:       fmt.Sprintf("%s - %s is now available", book.Author, book.Title),
-			Description: book.Link,
-			Fields: []dtos.EmbedField{
-				{
-					Name:   "Price",
-					Value:  book.Price,
-					Inline: false,
-				},
-			},
-			Color:     0x03fc90,
-			Timestamp: time.Now().Format(time.RFC3339),
-			// Thumbnail: dtos.EmbedImage{
-			// 	URL: book.Cover,
-			// },
-			Image: dtos.EmbedImage{
-				URL: book.Cover,
-			},
-			Footer: dtos.EmbedFooter{
-				Text:    "https://github.com/IamCathal/BooksBooksBooks",
-				IconURL: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-			},
-		}},
+	messageFormat := db.GetDiscordMessageFormat()
+	if messageFormat == "big" {
+		message.Embed[0].Thumbnail = dtos.EmbedImage{}
+		message.Embed[0].Image = dtos.EmbedImage{
+			URL: book.Cover,
+		}
 	}
-	DeliverWebHook(biggerStyleMessage, getDefaultWebhookURL())
+
+	DeliverWebHook(message, getDefaultWebhookURL())
 }
 
 func DeliverWebHook(msg dtos.DiscordMsg, webhookURL string) {
