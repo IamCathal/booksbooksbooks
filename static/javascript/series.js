0bcb1812a87ad92bdc3370f86f5d3c88cde3c31e
IamCathal <cathalsocallaghan@gmail.com>
2023-01-01 17:46:58 +0000

    Fix loading rendering for series books

--- static/javascript/series.js
+++ static/javascript/series.js
@@ -1,6 +1,6 @@
 giveSwayaaangBordersToItems()
 
-getAndRenderseriesCrawlIfPossible()
+getAndRenderSeriesCrawlIfPossible()
 
 function initWsConnection() {
     let booksFound = 0
@@ -18,7 +18,7 @@ function initWsConnection() {
         }
 
         if (isNewSeriesFoundMessage(msg)) {
-            renderSeries(msg.series)
+            renderSeriesForCurrentCrawl(msg.series)
         }
 
         if (isSearchResultMesage(msg)) {
@@ -91,18 +91,17 @@ function getCurrentHostname() {
     return new URL(window.location.href).host
 }
 
-function getAndRenderseriesCrawlIfPossible() {
+function getAndRenderSeriesCrawlIfPossible() {
     getSeriesCrawl().then((seriesCrawl) => {
-        console.log(seriesCrawl)
         seriesCrawl.forEach(series => {
-            renderSeries(series)
+            renderSeriesForExistingCrawl(series)
         })
     }, (err) => {
         console.error(seriesCrawl)
     })
 }
 
-function renderSeries(seriesInfo) {
+function renderSeriesForCurrentCrawl(seriesInfo) {
     document.getElementById("seriesRowsOutputDiv").innerHTML += 
     `
     <div class="row thinBorderBox mb-2 pl-2 pr-2 pt-1 pb-1">
@@ -118,20 +117,40 @@ function renderSeries(seriesInfo) {
                 </div>
             </div>
             <div class="row" style="width: 100%;">
-                ${fillInEachBookInSeries(seriesInfo.books)}
+                ${renderBooksInSeriesForCurrentCrawl(seriesInfo.books)}
+            </div>
+        </div>
+    </div>
+    `
+
+}
+function renderSeriesForExistingCrawl(seriesInfo) {
+    document.getElementById("seriesRowsOutputDiv").innerHTML += 
+    `
+    <div class="row thinBorderBox mb-2 pl-2 pr-2 pt-1 pb-1">
+        <div class="col">
+            <div class="row">
+                <div class="col pl-0" style="font-weight: bold; font-size: 1.2rem">
+                    <a href="${seriesInfo.link}">${seriesInfo.title}</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col pl-0">
+                    ${seriesInfo.author} - ${seriesInfo.primaryWorks} primary and ${seriesInfo.totalWorks} total works
+                </div>
+            </div>
+            <div class="row" style="width: 100%;">
+                ${renderSeriesBooksForExistingCrawl(seriesInfo.books)}
             </div>
         </div>
     </div>
     `
 }
 
-function fillInEachBookInSeries(books) {
+function renderSeriesBooksForExistingCrawl(books) {
     let output = ""
 
     books.forEach(book => {
-        const searchResultText = getSearchResultText(book.bookInfo, book.theBookshopMatch)
-        const matchFound = searchResultText == "No match found" ? false : true
-
         output += 
         `
         <div class="col-3 pt-2 pb-2" style="height: 6.8rem">
@@ -142,7 +161,7 @@ function fillInEachBookInSeries(books) {
                 <div class="col-9" style="height: 6rem">
                     <div class="row">
                         <div class="col">
-                            <p class="mb-0 seriesBookTitleOverflow" style="font-weight: bold"> ${book.bookInfo.title} </p>
+                            <p class="mb-0 seriesBookTitleOverflow" style="font-weight: bold" title="${book.bookInfo.title}"> ${book.bookInfo.title} </p>
                         </div>
                     </div>
                     <div class="row">
@@ -156,8 +175,48 @@ function fillInEachBookInSeries(books) {
                         </div>
                     </div>
                     <div class="row pl-4" style="position: absolute; bottom: 0.5rem; left: 0;  width: 100%">
-                        <div class="col-11 mt-0 thinBorderBox text-center" style="font-size: 0.7rem; ${matchFound ? "" : "border-color: #74797b; color: #74797b"}" id="${book.bookInfo.id}-theBookshopResults">
-                            ${getSearchResultText(book.bookInfo, book.theBookshopMatch)}
+                        <div class="col-11 mt-0 thinBorderBox text-center" style="font-size: 0.7rem; ${hasSearchResult(book.theBookshopMatch) ? "" : "border-color: #74797b; color: #74797b"}" id="${book.bookInfo.id}-theBookshopResults">
+                            ${getSearchResultText(book.theBookshopMatch)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    })
+    return output
+}
+
+function renderBooksInSeriesForCurrentCrawl(books) {
+    let output = ""
+
+    books.forEach(book => {
+        output += 
+        `
+        <div class="col-3 pt-2 pb-2" style="height: 6.8rem">
+            <div class="row">
+                <div class="col-3 pl-2">
+                    <img src="${book.bookInfo.cover}" style="width: 3.75rem; height: 5.7rem;">
+                </div>
+                <div class="col-9" style="height: 6rem">
+                    <div class="row">
+                        <div class="col">
+                            <p class="mb-0 seriesBookTitleOverflow" style="font-weight: bold" title="${book.bookInfo.title}"> ${book.bookInfo.title} </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col" style="font-size: 0.7rem">
+                            ${book.bookSeriesText}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col mt-0" style="font-size: 0.7rem">
+                            ${book.bookInfo.rating} stars. Published in 2015
+                        </div>
+                    </div>
+                    <div class="row pl-4" style="position: absolute; bottom: 0.5rem; left: 0;  width: 100%">
+                        <div class="col-11 mt-0 thinBorderBox text-center ${hasSearchResult(book.theBookshopMatch) ? "" : "skeleton"}" style="font-size: 0.7rem; ${hasSearchResult(book.theBookshopMatch) ? "" : "border-color: #74797b; color: #74797b"}" id="${book.bookInfo.id}-theBookshopResults">
+                            ${getSearchResultText(book.theBookshopMatch)}
                         </div>
                     </div>
                 </div>
@@ -169,23 +228,20 @@ function fillInEachBookInSeries(books) {
 }
 
 function renderSearchResult(searchBook, searchResult) {
-    const searchResultText = getSearchResultText(searchBook, searchResult)
-    if (searchResultText != "No match found") {
-        document.getElementById(`${searchBook.id}-theBookshopResults`).style.backgroundColor = "#48b348"
-    }
-    document.getElementById(`${searchBook.id}-theBookshopResults`).innerHTML = searchResultText
+    document.getElementById(`${searchBook.id}-theBookshopResults`).innerHTML = getSearchResultText(searchResult)
+    document.getElementById(`${searchBook.id}-theBookshopResults`).classList.remove("skeleton")
 }
 
-function getSearchResultText(searchBook, searchResult) {
-    console.log(searchResult)
-    if (searchResult.title === "") {
-        console.log("return nothing")
-        return "No match found"
-    } else {
-        console.log("return something")
-        // return `<a style="color: #22242f" href="${searchResult.link}">${searchResult.price} match found</a>`
+function getSearchResultText(searchResult) {
+    if (hasSearchResult(searchResult)) {
         return `<a style="font-weight: bold" href="${searchResult.link}">${searchResult.price} match found</a>`
+    } else {
+        return `<p class="mb-0 mt-0" style="color: #22242f">.</p>`
     }
+}
+
+function hasSearchResult(searchResult) {
+    return searchResult.title != ""
 }
 
 function updateStats(crawlStats) {
